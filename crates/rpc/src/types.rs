5fa3146e0e13530b55777ba1c917bf16edfe53a1
Péter Garamvölgyi <peter@scroll.io>
2024-10-17 20:49:40 +0200

    feat: remove proveBlock API

--- crates/rpc/src/types.rs
+++ crates/rpc/src/types.rs
@@ -4,28 +4,12 @@ use alloy::primitives::{Bytes, Signature, B256};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct ProveBlockRequest {
-    prev_state_root: B256,
-    block_trace: BlockTrace,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct ProveBlockResponse {
-    post_state_root: B256,
-    post_withdraw_root: B256,
-    signature: Signature,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ProveBatchRequest {
     prev_batch_header: Bytes,
+    prev_state_root: B256,
     batch_version: u8,
     // blocks
     // chunks
-    prev_state_root: B256,
-    state_roots: Vec<B256>,
-    withdraw_roots: Vec<B256>,
-    signatures: Vec<Signature>,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -38,6 +22,8 @@ pub struct ProveBatchResponse {
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ProveBundleRequest {
+    last_finalized_batch_header: Bytes,
+    prev_state_root: B256,
     batch_headers: Vec<Bytes>,
     state_roots: Vec<B256>,
     withdraw_roots: Vec<B256>,
